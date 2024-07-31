db650186fb78a89406b65f6043b6da51c037a87e
qyc <18115282782@163.com>
2024-07-31 15:37:16 +0800

    Add files via upload

---
+++ minllama-assignment-master/classifier.py
@@ -0,0 +1,61 @@
+
+import torch
+import torch.nn.functional as F
+
+# change it with respect to the original model
+from config import LlamaConfig
+from llama import load_pretrained
+from tokenizer import Tokenizer
+
+class LlamaZeroShotClassifier(torch.nn.Module):
+	def __init__(self, config: LlamaConfig, tokenizer: Tokenizer, label_names: list[str]):
+		super(LlamaZeroShotClassifier, self).__init__()
+		self.num_labels = config.num_labels
+		self.llama = load_pretrained(config.pretrained_model_path)
+		# Zero-shot classification does not require updating llama paramters.
+		for param in self.llama.parameters():
+			param.requires_grad = False
+		assert len(label_names) == self.num_labels
+		self.tokenizer = tokenizer
+		self.label_name_ids = [tokenizer.encode(label, bos=False, eos=False) for label in label_names]
+
+
+	def forward(self, input_ids):
+		# compute the completion probability of each label string
+		logits, _ = self.llama(input_ids)
+		log_probabilities = F.log_softmax(logits, dim=-1)
+		label_probabilities = torch.zeros((log_probabilities.shape[0], self.num_labels), device=log_probabilities.device)
+		for i, label_token_ids in enumerate(self.label_name_ids):
+			total_log_prob = torch.sum(log_probabilities[:, :, label_token_ids], axis=-1)
+			label_probabilities[:, i] = total_log_prob[:, 0]
+		return label_probabilities
+
+class LlamaEmbeddingClassifier(torch.nn.Module):
+	def __init__(self, config):
+		super(LlamaEmbeddingClassifier, self).__init__()
+		self.num_labels = config.num_labels
+		self.llama = load_pretrained(config.pretrained_model_path)
+		# If we use pretrain mode, we freeze Llama parameters.
+		for param in self.llama.parameters():
+			if config.option == 'pretrain':
+				param.requires_grad = False
+			elif config.option == 'finetune':
+				param.requires_grad = True
+
+		self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
+		self.classifier_head = torch.nn.Linear(self.llama.config.dim, self.num_labels)
+
+	def forward(self, input_ids):
+		# 获取最后一个token的隐藏状态
+		logits, hidden_states = self.llama(input_ids)
+		last_hidden_state = hidden_states[:, -1, :]
+
+		# 应用dropout
+		dropped_out = self.dropout(last_hidden_state)
+
+		# 通过分类头
+		logits = self.classifier_head(dropped_out)
+
+		# 返回log概率
+		log_probabilities = F.log_softmax(logits, dim=-1)
+		return log_probabilities
